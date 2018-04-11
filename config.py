4cc5a9548f8789751deb89dda883578885ab964b
varunbalupuri <varunbalupuri@gmail.com>
2018-04-11 19:13:02 +0100

    optional config

---
+++ config.py
@@ -0,0 +1,14 @@
+#config.py
+
+WHITELIST = {'london': ['the yard',
+                        'corsica studios',
+                        'bloc (autumn street)',
+                        'steelyard',
+                        'studio spaces e2',
+                        'brewhouse',
+                        'bunker club',
+                        "canavan's peckham",
+                        "clf arts cafe",
+                        "clf art cafe [block a, bussey building]"]
+            }
+
